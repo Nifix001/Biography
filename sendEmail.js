76cee958348ab6a1a970ef44ee0a495d7088a4ac
MrLad01 <ladayodeji@gmail.com>
2023-12-24 19:04:49 +0100

    update

--- sendEmail.js
+++ sendEmail.js
@@ -1,24 +1,41 @@
+const express = require('express');
 const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
-const transporter = nodemailer.createTransport({
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/send-email', (req, res) => {
+  const { email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'your_email@gmail.com', // Your email address
-        pass: 'your_password' // Your email password or an application-specific password
+      user: 'ladayodeji@gmail.com',
+      pass: 'Obafemi123',
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'ladayodeji@gmail.com',
+    subject: 'New Message from Your Website',
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
     }
+  });
 });
 
-const mailOptions = {
-    from: 'your_email@gmail.com',
-    to: 'recipient_email@example.com', // Recipient's email address
-    subject: 'New Message from Your Website',
-    text: 'This is a test message from your website.'
-};
-
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
